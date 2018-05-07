84dc0f41ceac62bbf59cd1dbaba545c37d2525d5
yvescallaert <yves.callaert@maersk.com>
2018-05-07 17:38:43 +0200

    configuring topic

--- dummyproducer/Config/configuration.py
+++ dummyproducer/Config/configuration.py
@@ -1,9 +1,9 @@
 config = {
     'local': {
-        'bootstrap_servers': 'localhost:9092',
+        'bootstrap_servers': 'localhost:9093',
         'kafkaMaxRequestSize': 50331648,
         'kafkaMaxMessageBytes': 50331648,
-        'kafka_produce_topic': 'YvesTestDataLoad',
+        'kafka_produce_topic': 'GunViolence',
         'fetch.wait.max.ms': 60000,
         'session.timeout.ms': 180000,
         'heartbeat.interval.ms': 60000,
